4165df9ce2d2d944a880241f745f357e613360f3
Muhammad Maaz <maazm6387@gmail.com>
2025-05-11 13:38:29 +0500

    updates

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -501,9 +501,11 @@ const Index = () => {
                                       <CreativeThumbnails creatives={creatives} />
                                     </div>
                                   ) : (
-                                    <div className="flex items-center gap-2 text-gray-400 cursor-pointer hover:text-blue-500 transition-colors duration-200" title="Click to refresh creatives">
+                                    <div className="flex items-center gap-2 text-gray-400 cursor-pointer hover:text-blue-500 transition-colors duration-200" >
                                       <Film className="w-4 h-4" />
-                                      <span>Loading creatives...</span>
+                                      <div><h5>Something went wrong</h5>
+                                      <h6>Please wait a few minutes before you try again.</h6>
+                                      </div>
                                       {/* <RefreshCw className="w-3 h-3 ml-1" /> */}
                                     </div>
                                   )}
